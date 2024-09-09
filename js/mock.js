1c5b08c35b8a2a5a01a3f9551c88ebb7be4551de
LeviGomes <levidebritogomes@gmail.com>
2024-09-09 16:40:17 -0300

    Ifood_Challenger_Fiap2024

---
+++ js/mock.js
@@ -0,0 +1,82 @@
+export const pagamentos = [
+    {
+      transacao: '#876364',
+      tipo: 'Saque',
+      conta: '<img src="../images/nubank-95.png" class="banco-icon" alt=""> 60078144-9',
+      valor: 'R$ 2.423,35',
+      data: '15/06/2024',
+      status: 'Concluída'
+    },
+    {
+      transacao: '#876234',
+      tipo: 'Saque',
+      conta: '<img src="../images/Itau-logo-0BE09A6D22-seeklogo.com.png" class="banco-icon" alt=""> 01959059-1',
+      valor: 'R$ 6.346,35',
+      data: '14/06/2024',
+      status: 'Pendente'
+    },
+    {
+      transacao: '#876164',
+      tipo: 'Saque',
+      conta: '<img src="../images/nubank-95.png" class="banco-icon" alt=""> 60078144-9',
+      valor: 'R$ 523,99',
+      data: '12/06/2024',
+      status: 'Concluída'
+    },
+    {
+      transacao: '#876012',
+      tipo: 'Saque',
+      conta: '<img src="../images/nubank-95.png" class="banco-icon" alt=""> 60078144-9',
+      valor: 'R$ 1.140,00',
+      data: '12/06/2024',
+      status: 'Cancelada'
+    },
+    {
+      transacao: '#871536',
+      tipo: 'Transferência',
+      conta: '<img src="../images/nubank-95.png" class="banco-icon" alt=""> 60078144-9',
+      valor: 'R$ 650,00',
+      data: '10/06/2024',
+      status: 'Concluída'
+    },
+    {
+      transacao: '#871233',
+      tipo: 'Saque',
+      conta: '<img src="../images/nubank-95.png" class="banco-icon" alt=""> 60078144-9',
+      valor: 'R$ 157,65',
+      data: '9/06/2024',
+      status: 'Concluída'
+    },
+    {
+      transacao: '#870235',
+      tipo: 'Saque',
+      conta: '<img src="../images/nubank-95.png" class="banco-icon" alt=""> 60078144-9',
+      valor: 'R$ 800,00',
+      data: '7/06/2024',
+      status: 'Pendente'
+    },
+    {
+      transacao: '#869534',
+      tipo: 'Saque',
+      conta: '<img src="../images/Itau-logo-0BE09A6D22-seeklogo.com.png" class="banco-icon" alt=""> 01959059-1',
+      valor: 'R$ 120,50',
+      data: '6/06/2024',
+      status: 'Concluída'
+    },
+    {
+      transacao: '#867453',
+      tipo: 'Saque',
+      conta: '<img src="../images/Itau-logo-0BE09A6D22-seeklogo.com.png" class="banco-icon" alt=""> 01959059-1',
+      valor: 'R$ 50,00',
+      data: '3/06/2024',
+      status: 'Concluída'
+    },
+    {
+      transacao: '#867143',
+      tipo: 'Saque',
+      conta: '<img src="../images/nubank-95.png" class="banco-icon" alt=""> 60078144-9',
+      valor: 'R$ 1.200,00',
+      data: '1/06/2024',
+      status: 'Cancelada'
+    }
+  ];
